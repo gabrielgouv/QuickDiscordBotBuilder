c770ae6d0161fbdc9ff43821115ffbcfc64ed8e8
João Gabriel Gouveia de Almeida <gabrielgouv@Joaos-MacBook-Pro.local>
2018-11-21 00:06:56 -0300

    Added Prefixed Message Processor

---
+++ src/message/prefixed-message-processor.ts
@@ -0,0 +1,29 @@
+import { MessageProcessor } from "./message-processor";
+import { DiscordBot } from "../bot/discord-bot";
+import { Message } from "discord.js";
+
+export class PrefixedMessageProcessor extends MessageProcessor {
+
+    public constructor(protected bot: DiscordBot, private prefix: string) {
+        super(bot);
+    }
+
+    process(message: Message): { trigger: string; args: string[]; } {
+
+        const messageContent = message.content;
+
+        let splittedMessage = messageContent.split(' ');
+
+        let trigger : string = '';
+        let args : string[] = [];
+
+        if(splittedMessage[0] === this.prefix){
+            trigger = splittedMessage[1];
+            args = splittedMessage.splice(2, splittedMessage.length);
+        }
+
+        return { trigger: trigger, args: args }
+
+    }    
+    
+}
